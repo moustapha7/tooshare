9b1dd187d28ad5def24ff4ab5166f5e3897ee80d
Unknown <elyroot94@gmail.com>
2018-05-01 03:32:49 +0100

    Ajout de Validation Register login cote Backend

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,13 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    protected function respondWithToken($data,$token)
+
+
+    /**
+     * AuthController constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    protected function respondWithToken($data, $token)
     {
         return response()->json([
-            'Accept'=> 'application/json',
             'access_token' => $token,
             'data'=>$data,
             'token_type' => 'bearer',
@@ -20,7 +29,16 @@ class AuthController extends Controller
     }
     public function Register(Request $request)
     {
-
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|unique:users',
+            'first_name' => 'required',
+            'last_name'=>'required',
+            'password'=> 'required|min:6',
+            'phone'=>'required|alpha_num|unique:users|min:6'
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
 
         $user=User::create([
             'first_name' => $request->first_name,
@@ -36,6 +54,13 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password'=> 'required|min:6'
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
         $credentials = $request->only(['email', 'password']);
 
         if (!$token = auth()->attempt($credentials)) {
