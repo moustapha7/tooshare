07372b3950b15d1eac9d558f9c6ecd3e75654692
Moussa Diatta <moussa@helixtechno.com>
2018-07-26 17:09:23 +0000

    MAJ

---
+++ resources/assets/js/components/home/Emoji.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import EmojiPicker from 'emoji-picker-react';
+
+export default class Emoji extends Component {
+    render() {
+        return (
+            <EmojiPicker onEmojiClick={myCallback}/>
+        );
+    }
+}
